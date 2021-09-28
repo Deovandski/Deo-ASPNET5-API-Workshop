bb6eb43b499f2a0e40fa21041abcb921be910f94
Deovandski Skibinski Junior <deovandski@outlook.com>
2021-09-28 11:07:37 -0500

    DT1-401 Following Module 1-04

--- ChinookASPNETWebAPI/ChinookASPNETWebAPI.API/Program.cs
+++ ChinookASPNETWebAPI/ChinookASPNETWebAPI.API/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ServingTheHues
+namespace ChinookASPNETWebAPI.API
 {
     public class Program
     {
--- ChinookASPNETWebAPI/ChinookASPNETWebAPI.API/Startup.cs
+++ ChinookASPNETWebAPI/ChinookASPNETWebAPI.API/Startup.cs
@@ -6,13 +6,12 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ServingTheHues
+namespace ChinookASPNETWebAPI.API
 {
     public class Startup
     {
@@ -28,10 +27,6 @@ namespace ServingTheHues
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ServingTheHues", Version = "v1" });
-            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +35,6 @@ namespace ServingTheHues
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ServingTheHues v1"));
             }
 
             app.UseHttpsRedirection();
